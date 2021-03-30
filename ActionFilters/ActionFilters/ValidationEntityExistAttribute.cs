5eecde7a5a2fe376cf7488ee1eb3b7bfb652b247
Mahmoud Nasr <ma7moud.asp@gmail.com>
2021-03-30 18:28:12 +0200

    init

---
+++ ActionFilters/ActionFilters/ValidationEntityExistAttribute.cs
@@ -0,0 +1,48 @@
+﻿using ActionFilters.Contracts;
+using ActionFilters.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilters.ActionFilters
+{
+    public class ValidationEntityExistAttribute<T> : IActionFilter where T : class,IEntity
+    {
+        private readonly MovieContext dbcontext;
+
+        public ValidationEntityExistAttribute(MovieContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var id = Guid.Empty;
+            if (context.ActionArguments.ContainsKey("id"))
+            {
+                id = (Guid)context.ActionArguments["id"];
+               
+            }
+            else
+            {
+                context.Result = new BadRequestObjectResult("Object is null");
+            }
+            var entity = dbcontext.Set<T>().SingleOrDefault(x => x.Id.Equals(id));
+            if (entity == null)
+            {
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("entity", entity);
+            }
+        }
+    }
+}
---
+++ ActionFilters/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using ActionFilters.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilters.ActionFilters
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+       
+        }
+
+        void IActionFilter.OnActionExecuting(ActionExecutingContext context)
+        {
+            var param = context.ActionArguments.FirstOrDefault(i => i.Value is IEntity);
+            if (param.Value == null)
+            {
+                context.Result = new BadRequestObjectResult("Object is null");
+            }
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
